15606d8d73a6100111f2c2dc5c21cc3c7456f663
Brian Giori <briangiori@gmail.com>
2018-12-28 09:56:59 -0800

    Got cocoapods working

--- Example/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
@@ -153,9 +153,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/McuManager/McuManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftCBOR/SwiftCBOR.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/McuManager/McuManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftCBOR/SwiftCBOR.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
