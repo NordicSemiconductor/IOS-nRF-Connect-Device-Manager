e1317edb57a59f143be315a472bb2f84b4afd3ab
Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
2022-04-14 11:33:46 -0500

    Squashed commit of the following:
    commit f29a4174371e602ede103ac8b17367f57ea6c69f
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:44:04 2022 -0500
    
        Update Cartfile
    
    commit addcb198cd76cab538d47ea263193214b1e76081
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:36:41 2022 -0500
    
        Update Cartfile
    
    commit ed2a73eaa3cf7d6a1b59505b5577c4da2fe5381a
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:19:21 2022 -0500
    
        Adding pods files to support carthage.
    
    commit 59d9e1bc8daa07bed514e5189f16f4a27ba4aafb
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:06:24 2022 -0500
    
        More
    
    commit 88981d91ba10b37eb83a42784c09d93b41d9b4ca
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:04:16 2022 -0500
    
        Create McuManager.xcscheme
    
    commit a953dc142b07ec3d1eb53f654bc78696ccdc6aaf
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 18:01:36 2022 -0500
    
        Delete IDEWorkspaceChecks.plist
    
    commit 85ab09e81952ee4f7fb9c83ecd3458f3f01f996b
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 17:56:02 2022 -0500
    
        Upgrades to support carthage
    
    commit 4acca9e7af613c6793cfcad89479c38f0851dbe3
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 17:33:17 2022 -0500
    
        Update Cartfile
    
    commit aab72305b894277a012fb790f1037db9ac14174d
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 17:32:52 2022 -0500
    
        Update Cartfile
    
    commit ee276227ca8eb78f3963fd89a3a25ad761822b71
    Author: Griffin Meyer <100242167+GriffinJBC@users.noreply.github.com>
    Date:   Wed Apr 13 17:30:00 2022 -0500
    
        Adding to scheme

---
+++ Example/Pods/Target Support Files/Pods-nRF Connect Device Manager/Pods-nRF Connect Device Manager-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_nRF_Connect_Device_ManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_nRF_Connect_Device_ManagerVersionString[];
+
